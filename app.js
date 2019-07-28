6ebcf0883bd29cae1338e06750fb34fb21c4a232
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-07-28 21:04:39 +0100

    App configuration

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var express     =   require("express");
 
     mongoose.connect("mongodb://localhost:27017/blog_app", { useNewUrlParser: true });
 
+// App configuration
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("The server has Started");
 })
