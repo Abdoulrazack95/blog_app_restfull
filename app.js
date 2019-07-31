674e10f6cfc3f8e794284f4c8aaeb99c41763af3
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-07-31 16:58:12 +0100

    styling the index page done

--- app.js
+++ app.js
@@ -48,9 +48,7 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
     //create blog
-    console.log(req.body)
     req.body.blog.body = req.sanitize(req.body.blog.body);
-    console.log("===========");
     console.log(req.body);
     blog.create(req.body.blog, function(err, newblog){
         if(err){
